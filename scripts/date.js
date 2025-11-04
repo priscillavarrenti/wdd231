4b15cda0e6b2bb414f41983320f92a3054fcc6f1
priscillavarrenti <priscillavarrentilds@gmail.com>
2025-11-04 00:10:22 -0300

    font and date

--- scripts/date.js
+++ scripts/date.js
@@ -3,7 +3,4 @@ if (currentYearSpan) {
     currentYearSpan.textContent = new Date().getFullYear();
 }
 
-const lastModifiedParagraph = document.getElementById('lastModified');
-if (lastModifiedParagraph) {
-    lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
-}
+document.getElementById("lastModified").innerHTML=document.lastModified;
