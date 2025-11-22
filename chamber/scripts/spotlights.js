969fc81c35c05a845ea73ecdfd80bb7da0fb3f07
priscillavarrenti <priscillavarrentilds@gmail.com>
2025-11-22 20:48:23 -0300

    correct error

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -1,17 +1,34 @@
-const spotlightsContainer = document.querySelector("#spotlights-container");
-const membersURL = "data/members.json";
+const spotlightContainer = document.querySelector("#spotlights-container");
 
-async function getMembers() {
-    try {
-        const response = await fetch(membersURL);
-        const data = await response.json();
+async function loadSpotlights() {
+    try{
+        const response = await fetch("data/members.json");
+        const data= await response.json();
 
-        const members = data.members.map(member => ({
-            ...member,
-            membership:
-                member.membershipLevel === 3
-                    ? "Gold"
-        }))
+        const filteredMembers = data.members.filter(member =>
+            member.membershipLevel === 2 || member.membershipLevel === 3
+        );
+
+        const shuffled = filteredMembers.sort(() => 0.5 - Math.random());
+
+        const spotlightCount = Math.random()<0.5 ? 2 : 3;
+        const selected = shuffled.slice(0, spotlightCount);
+
+        displaySpotLights(selected);
+    } catch (error) {
+        console.error("Error loading spotlights:", error);
     }
     
 }
+ function displaySpotLights(members) {
+    spotlightContainer.innerHTML ="";
+    members.forEach(member => {
+        const card = document.createElement("div");
+        card.classList.add("spotlight-card");
+
+        card.innerHTML = `
+        <img src= "${member.image}" alt="${member.name}">
+        `;
+        
+    });
+ }
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -36,3 +36,55 @@ function displayResults(data) {
     captionDesc.textContent = `${desc}`;
 }
 apiFetch();
+
+const forecastContainer = document.querySelector("#forecast");
+
+const  forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=metric`;
+
+async function fetchForecast() {
+    try{
+        const response = await fetch(forecastURL);
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Forecast:", data);
+
+            const middayData = data.list.filter(item => item.dt_txt.includes("12:00:00"));
+
+            displayForecast(middayData);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayForecast(data) {
+    forecastContainer.innerHTML= "";
+
+    const threeDays = data.slice(0, 3);
+
+    threeDays.forEach(day => {
+        const date = new Date(day.dt_txt).toLocaleDateString("en-US", {
+            weekday: "long"
+        });
+
+        const temp = day.main.temp.toFixed(1);
+        const desc = day.weather[0].description;
+        const icon = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+
+        const card = document.createElement("div");
+        card.classList.add("forecast-day");
+
+        card.innerHTML= `
+            <h4>${date}</h4>
+            <img src="${icon}" alt="${desc}">
+            <p>${desc}</p>
+            <p>${temp}°C</p>
+        `;
+        forecastContainer.appendChild(card);
+    });
+}
+
+fetchForecast();
